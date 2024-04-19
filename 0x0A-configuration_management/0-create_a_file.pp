e28000d6c55555cbfb778a586927dbcd310bd376
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-04-19 10:58:05 +0200

    task 0 done

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# A manifest the create a file in tmp folder
+
+file {'/tmp/school':
+    ensure => file,
+    owner => 'www-data',
+    group => 'www-data',
+    mode => '0744',
+    content => 'I love Puppet'
+}
