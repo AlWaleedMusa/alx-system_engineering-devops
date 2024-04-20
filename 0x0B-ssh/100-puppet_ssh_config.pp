a677c5ea41ba660d7329e9e644d8fe56fc364619
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-04-20 17:22:17 +0200

    task 100 done

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,21 +1,14 @@
 # puppet ssh config
-
+y
 include stdlib
 
 file_line { 'password_authentication':
     ensure  => present,
-    path   => '.ssh/ssh_config',
+    path   => 'etc/ssh/ssh_config',
     content => '
         Host *
             IdentityFile ~/.ssh/school
             PasswordAuthentication no
     ',
     replace => true,
-}
-
-file_line { 'identity':
-  ensure => present,
-  path   => '.ssh/ssh_config',
-  line   => '     IdentityFile ~/.ssh/school',
-  replace => true,
 }
