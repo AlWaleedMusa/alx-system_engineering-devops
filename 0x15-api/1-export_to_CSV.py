e2ed940fafb274500516b089186e85df3ea8f8af
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-05-21 00:52:40 +0300

    task 1 done

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""using task 0 extend your Python script to export data in the CSV format"""
+
+import requests
+import sys
+import csv
+
+
+if __name__ == "__main__":
+    """Prints the to-do list for a given employee ID."""
+
+    if len(sys.argv) != 2:
+        print("Usage: <{}> <employee_id>".format(sys.argv[0]))
+        sys.exit()
+
+    api = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(api + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(api + "todos", params={"userId": sys.argv[1]}).json()
+
+    completed = [t.get("title") for t in todos if t.get("completed")]
+    with open("{}.csv".format(sys.argv[1]), "w") as csv_file:
+        writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+        [writer.writerow([sys.argv[1], user.get("username"), t.get("completed"), t.get("title")]) for t in todos]
