0e8f0f43e1ca4fec4ee534b0fc9170e58fd17ce5
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-06-04 19:45:42 +0300

    task 0 done

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Returns the number of subscribers"""
+
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        return response.json().get("data").get("subscribers")
+    else:
+        return 0
