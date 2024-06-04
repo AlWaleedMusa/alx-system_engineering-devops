abc0a1a36809f183f4796bb987748f1e32e36ff5
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-06-04 19:58:26 +0300

    task 100 done

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import requests
+
+
+def count_words(subreddit, word_list, word_count={}, after=None):
+    """Counts the number of times a word appears in the titles of hot articles for a given subreddit"""
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?after={after}"
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        for post in response.json().get("data").get("children"):
+            title = post.get("data").get("title")
+            for word in word_list:
+                if word.lower() in title.lower():
+                    if word in word_count:
+                        word_count[word] += 1
+                    else:
+                        word_count[word] = 1
+
+        after = response.json().get("data").get("after")
+        if after is not None:
+            return count_words(subreddit, word_list, word_count, after)
+
+        if len(word_count) == 0:
+            return None
+
+        for word in sorted(word_count.keys()):
+            print(f"{word}: {word_count[word]}")
+    else:
+        print(None)
