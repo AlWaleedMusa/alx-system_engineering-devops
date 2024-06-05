124515bb045a631de9ca3192ad669b3ec1106d39
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-06-06 01:17:10 +0300

    task 0 fixed

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,13 @@
+# Ensure that the file '/etc/apache2/sites-available/your_site.conf' is present
 file { '/etc/apache2/sites-available/your_site.conf':
     ensure  => present,
     content => '# Your corrected virtual host configuration content here',
-    notify  => Service['apache2'],
+    notify  => Service['apache2'],  # Notify the 'apache2' service when the file changes
 }
 
+# Ensure that the 'apache2' service is running and enabled
 service { 'apache2':
     ensure  => running,
     enable  => true,
-    require => File['/etc/apache2/sites-available/your_site.conf'],
+    require => File['/etc/apache2/sites-available/your_site.conf'],  # Require the file '/etc/apache2/sites-available/your_site.conf' to be present before starting the service
 }
