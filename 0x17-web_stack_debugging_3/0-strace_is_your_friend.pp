e53e7f157004667231bfb6766b7c3a7da6c5259b
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-06-06 01:20:37 +0300

    fixed the code task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,15 +1,6 @@
-# A manifest the debug an apache issue
+# Fixes bad extensions in the WordPress file `wp-settings.php`.
 
-# Ensure that the file '/etc/apache2/sites-available/your_site.conf' is present
-file { '/etc/apache2/sites-available/your_site.conf':
-    ensure  => present,
-    content => '# Your corrected virtual host configuration content here',
-    notify  => Service['apache2'],  # Notify the 'apache2' service when the file changes
-}
-
-# Ensure that the 'apache2' service is running and enabled
-service { 'apache2':
-    ensure  => running,
-    enable  => true,
-    require => File['/etc/apache2/sites-available/your_site.conf'],  # Require the file '/etc/apache2/sites-available/your_site.conf' to be present before starting the service
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
